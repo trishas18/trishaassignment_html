71e0691ce09a7df43cab6a9f6ee3d2eca803574f
trishas18 <trishasudhakar2@gmail.com>
2023-07-24 19:19:48 +0530

    assignment update

---
+++ classasignmnet.js/buttonscript.js
@@ -0,0 +1,5 @@
+var myDiv = document.getElementById("myDiv");
+var myButton = document.getElementById("myButton");
+myButton.addEventListener("click", function() { 
+  myDiv.style.backgroundColor = "yellow";
+});
