358aa39bb72015ee410df5a996e20ca92811329a
trishas18 <135442801+trishas18@users.noreply.github.com>
2023-08-24 19:43:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Employee from './components/Employee'; 
+import EmployeeList from './components/Employeelist'; 
+import User from './components/User.js';
+
+import Customer from './components/Customer';
+function App(){
+
+  return ( 
+      
+    <>
+    <EmployeeList /> 
+    <User />
+    <Customer />
+   
+    
+  
+      </>
+  
+  )
+  
+  
+  }
+  
+  export default App;
